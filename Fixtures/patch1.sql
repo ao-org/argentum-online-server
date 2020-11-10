240405f864497caf027e9f5f43c925b6fd466f27
jopi <jopi@d3445b43-944b-e54e-ad87-82f32d028447>
2020-11-10 01:01:58 +0000

    Saco el patch del fixture.sql (ya que vamos a hacer reset anyway). Meto los cambios que habian en el patch1.sql en fixture.sql

--- Fixtures/patch1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `statistics` (
-  `name` VARCHAR(50) NOT NULL,
-  `value` VARCHAR(50) NULL,
-  PRIMARY KEY (`name`))
-ENGINE = InnoDB;
-
-INSERT INTO `statistics` (`name`, `value`) VALUES ('online', '0');
-
-ALTER TABLE `account` CHANGE COLUMN `is_logged` `logged` INT(11) NULL DEFAULT 0;
