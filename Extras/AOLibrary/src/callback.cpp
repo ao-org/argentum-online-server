f2238f11c20d4bfa6f80003ea0ee92175799869e
Joaquin <jopiodz00@gmail.com>
2021-05-29 22:09:08 -0300

    pequeño retoque

--- Extras/AOLibrary/src/callback.cpp
+++ Extras/AOLibrary/src/callback.cpp
@@ -2,13 +2,12 @@
 #include <stdint.h>
 
 EXPORT void CALLBACK CallHandle(DWORD address, int16_t userindex) {
-    typedef void(__stdcall* SubUI)(int16_t userindex);
-    typedef void(__stdcall* SubSinUI)();
-
     if (userindex) {
+        typedef void(CALLBACK* SubUI)(int16_t userindex);
         SubUI FunctionCall = reinterpret_cast<SubUI>(address);
         FunctionCall(userindex);
     } else {
+        typedef void(CALLBACK* SubSinUI)();
         SubSinUI FunctionCall = reinterpret_cast<SubSinUI>(address);
         FunctionCall();
     }
