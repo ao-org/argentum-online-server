54d018e7b0c3b1005e17025cb06f56bfc898237d
Joaquin <jopiodz00@gmail.com>
2021-05-31 15:35:58 -0300

    Muevo codigo de la AOLibrary a un repo aparte.

--- Extras/AOLibrary/src/include/declares.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-#define WIN32_LEAN_AND_MEAN // Excluir material rara vez utilizado de encabezados de Windows
-#define VC_EXTRALEAN        // Excluir material rara vez utilizado de encabezados de Windows (de MSVC)
-
-#define EXPORT extern "C" __declspec(dllexport)
