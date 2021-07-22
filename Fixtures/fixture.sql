071fc522aca54ac4d2245c87698136841e250357
Lucas <lucas.recoaro@gmail.com>
2021-07-23 11:14:15 +1200

    Update fixture.sql

--- Fixtures/fixture.sql
+++ Fixtures/fixture.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-01-2021 a las 04:26:22
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 23-07-2021 a las 01:07:42
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ao_server_prod`
 --
-CREATE DATABASE IF NOT EXISTS `ao_server_prod` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `ao_server_prod`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +27,6 @@ USE `ao_server_prod`;
 -- Estructura de tabla para la tabla `account`
 --
 
-DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `id` mediumint(8) UNSIGNED NOT NULL,
   `email` varchar(320) NOT NULL,
@@ -60,18 +57,19 @@ CREATE TABLE `account` (
 --
 -- Estructura de tabla para la tabla `attribute`
 --
-DROP TABLE IF EXISTS `attribute`;
+
 CREATE TABLE `attribute` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
   `value` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `bank_item`
 --
-DROP TABLE IF EXISTS `bank_item`;
+
 CREATE TABLE `bank_item` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
@@ -79,20 +77,24 @@ CREATE TABLE `bank_item` (
   `amount` smallint(5) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `connection`
 --
-DROP TABLE IF EXISTS `connection`;
+
 CREATE TABLE `connection` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `ip` varchar(16) NOT NULL,
   `date_last_login` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `house_key`
 --
-DROP TABLE IF EXISTS `house_key`;
+
 CREATE TABLE `house_key` (
   `key_obj` smallint(5) UNSIGNED NOT NULL,
   `account_id` mediumint(8) UNSIGNED NOT NULL,
@@ -104,7 +106,7 @@ CREATE TABLE `house_key` (
 --
 -- Estructura de tabla para la tabla `inventory_item`
 --
-DROP TABLE IF EXISTS `inventory_item`;
+
 CREATE TABLE `inventory_item` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
@@ -113,13 +115,12 @@ CREATE TABLE `inventory_item` (
   `is_equipped` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `mail`
 --
-DROP TABLE IF EXISTS `mail`;
+
 CREATE TABLE `mail` (
   `id` mediumint(8) UNSIGNED NOT NULL,
   `user_id` mediumint(8) UNSIGNED NOT NULL,
@@ -133,24 +134,133 @@ CREATE TABLE `mail` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `mao__claims`
+--
+
+CREATE TABLE `mao__claims` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `deal_id` int(11) UNSIGNED NOT NULL,
+  `claimer_account_id` mediumint(8) UNSIGNED NOT NULL,
+  `claimer_character_id` mediumint(8) UNSIGNED DEFAULT NULL,
+  `gold_amount` int(11) NOT NULL,
+  `claimed_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mao__deals`
+--
+
+CREATE TABLE `mao__deals` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `publisher_account_id` mediumint(8) UNSIGNED NOT NULL,
+  `character_id` mediumint(8) UNSIGNED NOT NULL,
+  `description` varchar(512) DEFAULT '',
+  `deal_type` varchar(11) NOT NULL COMMENT 'Any of `sell/trade`',
+  `gold_price` int(11) NOT NULL,
+  `closed_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `mao__deal_summaries`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `mao__deal_summaries` (
+`id` int(11) unsigned
+,`publisher_account_id` mediumint(8) unsigned
+,`character_id` mediumint(8) unsigned
+,`description` varchar(512)
+,`deal_type` varchar(11)
+,`gold_price` int(11)
+,`closed_at` timestamp
+,`created_at` timestamp
+,`updated_at` timestamp
+,`character_name` varchar(30)
+,`class_id` tinyint(3) unsigned
+,`race_id` tinyint(3) unsigned
+,`genre_id` tinyint(3) unsigned
+,`elo` int(11)
+,`level` smallint(5) unsigned
+,`head_id` smallint(5) unsigned
+,`max_hp` smallint(5) unsigned
+,`warnings` tinyint(3) unsigned
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mao__offers`
+--
+
+CREATE TABLE `mao__offers` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `deal_id` int(11) UNSIGNED NOT NULL,
+  `offeror_account_id` mediumint(8) UNSIGNED NOT NULL,
+  `character_id` mediumint(8) UNSIGNED NOT NULL,
+  `offer_price` int(11) NOT NULL,
+  `state` varchar(255) NOT NULL COMMENT 'Any of `pending/accepted/rejected/completed/cancelled`',
+  `description` varchar(512) DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `mao__offer_summaries`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `mao__offer_summaries` (
+`id` int(11) unsigned
+,`deal_id` int(11) unsigned
+,`offeror_account_id` mediumint(8) unsigned
+,`character_id` mediumint(8) unsigned
+,`offer_price` int(11)
+,`state` varchar(255)
+,`description` varchar(512)
+,`created_at` timestamp
+,`updated_at` timestamp
+,`character_name` varchar(30)
+,`class_id` tinyint(3) unsigned
+,`race_id` tinyint(3) unsigned
+,`genre_id` tinyint(3) unsigned
+,`elo` int(11)
+,`level` smallint(5) unsigned
+,`head_id` smallint(5) unsigned
+,`max_hp` smallint(5) unsigned
+,`warnings` tinyint(3) unsigned
+);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pet`
 --
-DROP TABLE IF EXISTS `pet`;
+
 CREATE TABLE `pet` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
   `pet_id` smallint(5) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `punishment`
 --
-DROP TABLE IF EXISTS `punishment`;
+
 CREATE TABLE `punishment` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `reason` varchar(255) NOT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP()
+  `created_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -158,7 +268,7 @@ CREATE TABLE `punishment` (
 --
 -- Estructura de tabla para la tabla `quest`
 --
-DROP TABLE IF EXISTS `quest`;
+
 CREATE TABLE `quest` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
@@ -167,58 +277,78 @@ CREATE TABLE `quest` (
   `npcstarget` varchar(64) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `quest_done`
 --
-DROP TABLE IF EXISTS `quest_done`;
+
 CREATE TABLE `quest_done` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `quest_id` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `ranking_users`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `ranking_users` (
+`id` mediumint(8) unsigned
+,`character_name` varchar(30)
+,`class_id` tinyint(3) unsigned
+,`race_id` tinyint(3) unsigned
+,`genre_id` tinyint(3) unsigned
+,`head_id` smallint(5) unsigned
+,`elo` int(11)
+,`level` smallint(5) unsigned
+,`exp` int(10) unsigned
+,`total_gold` bigint(11) unsigned
+,`total_kills` int(6) unsigned
+);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `skillpoint`
 --
-DROP TABLE IF EXISTS `skillpoint`;
+
 CREATE TABLE `skillpoint` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
   `value` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `spell`
 --
-DROP TABLE IF EXISTS `spell`;
+
 CREATE TABLE `spell` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `number` tinyint(3) UNSIGNED NOT NULL,
   `spell_id` smallint(5) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `statistics`
 --
-DROP TABLE IF EXISTS `statistics`;
+
 CREATE TABLE `statistics` (
   `name` varchar(50) NOT NULL,
   `value` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `statistics`
---
-
-INSERT INTO `statistics` (`name`, `value`) VALUES ('online', '0');
-INSERT INTO `statistics` (`name`, `value`) VALUES ('record', '0');
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `user`
 --
-DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` mediumint(8) UNSIGNED NOT NULL,
   `account_id` mediumint(8) UNSIGNED NOT NULL,
@@ -243,9 +373,7 @@ CREATE TABLE `user` (
   `pos_map` smallint(5) UNSIGNED NOT NULL,
   `pos_x` tinyint(3) UNSIGNED NOT NULL,
   `pos_y` tinyint(3) UNSIGNED NOT NULL,
-  `return_map` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
-  `return_x` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
-  `return_y` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
+  `last_map` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `body_id` smallint(5) UNSIGNED NOT NULL,
   `head_id` smallint(5) UNSIGNED NOT NULL,
   `weapon_id` smallint(5) UNSIGNED NOT NULL,
@@ -324,16 +452,49 @@ CREATE TABLE `user` (
   `guild_rejected_because` varchar(255) DEFAULT NULL,
   `chat_global` tinyint(1) DEFAULT 1,
   `chat_combate` tinyint(1) DEFAULT 1,
-  `warnings` tinyint(3) UNSIGNED NOT NULL DEFAULT 0
+  `warnings` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
+  `elo` int(11) NOT NULL DEFAULT 1000,
+  `return_map` int(11) NOT NULL DEFAULT 0,
+  `return_x` int(11) NOT NULL DEFAULT 0,
+  `return_y` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `whitelist`
 --
-DROP TABLE IF EXISTS `whitelist`;
+
 CREATE TABLE `whitelist` (
   `code` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `mao__deal_summaries`
+--
+DROP TABLE IF EXISTS `mao__deal_summaries`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `mao__deal_summaries`  AS SELECT `d`.`id` AS `id`, `d`.`publisher_account_id` AS `publisher_account_id`, `d`.`character_id` AS `character_id`, `d`.`description` AS `description`, `d`.`deal_type` AS `deal_type`, `d`.`gold_price` AS `gold_price`, `d`.`closed_at` AS `closed_at`, `d`.`created_at` AS `created_at`, `d`.`updated_at` AS `updated_at`, `u`.`name` AS `character_name`, `u`.`class_id` AS `class_id`, `u`.`race_id` AS `race_id`, `u`.`genre_id` AS `genre_id`, `u`.`elo` AS `elo`, `u`.`level` AS `level`, `u`.`head_id` AS `head_id`, `u`.`max_hp` AS `max_hp`, `u`.`warnings` AS `warnings` FROM (`mao__deals` `d` join `user` `u` on(`d`.`character_id` = `u`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `mao__offer_summaries`
+--
+DROP TABLE IF EXISTS `mao__offer_summaries`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `mao__offer_summaries`  AS SELECT `o`.`id` AS `id`, `o`.`deal_id` AS `deal_id`, `o`.`offeror_account_id` AS `offeror_account_id`, `o`.`character_id` AS `character_id`, `o`.`offer_price` AS `offer_price`, `o`.`state` AS `state`, `o`.`description` AS `description`, `o`.`created_at` AS `created_at`, `o`.`updated_at` AS `updated_at`, `u`.`name` AS `character_name`, `u`.`class_id` AS `class_id`, `u`.`race_id` AS `race_id`, `u`.`genre_id` AS `genre_id`, `u`.`elo` AS `elo`, `u`.`level` AS `level`, `u`.`head_id` AS `head_id`, `u`.`max_hp` AS `max_hp`, `u`.`warnings` AS `warnings` FROM (`mao__offers` `o` join `user` `u` on(`o`.`character_id` = `u`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `ranking_users`
+--
+DROP TABLE IF EXISTS `ranking_users`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `ranking_users`  AS SELECT `u`.`id` AS `id`, `u`.`name` AS `character_name`, `u`.`class_id` AS `class_id`, `u`.`race_id` AS `race_id`, `u`.`genre_id` AS `genre_id`, `u`.`head_id` AS `head_id`, `u`.`elo` AS `elo`, `u`.`level` AS `level`, `u`.`exp` AS `exp`, `u`.`gold`+ `u`.`bank_gold` AS `total_gold`, `u`.`criminales_matados`+ `u`.`ciudadanos_matados` AS `total_kills` FROM `user` AS `u` WHERE `u`.`deleted` = 0 ;
 
 --
 -- Índices para tablas volcadas
@@ -385,6 +546,32 @@ ALTER TABLE `mail`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indices de la tabla `mao__claims`
+--
+ALTER TABLE `mao__claims`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_mao__claims_deal_id` (`deal_id`),
+  ADD KEY `FK_mao__claims_account_id` (`claimer_account_id`),
+  ADD KEY `FK_mao__claims_user_id` (`claimer_character_id`);
+
+--
+-- Indices de la tabla `mao__deals`
+--
+ALTER TABLE `mao__deals`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_account_id` (`publisher_account_id`),
+  ADD KEY `FK_user_id` (`character_id`);
+
+--
+-- Indices de la tabla `mao__offers`
+--
+ALTER TABLE `mao__offers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_mao__offers_deal_id` (`deal_id`),
+  ADD KEY `FK_mao__offers_account_id` (`offeror_account_id`),
+  ADD KEY `FK_mao__offers_user_id` (`character_id`);
+
+--
 -- Indices de la tabla `pet`
 --
 ALTER TABLE `pet`
@@ -431,6 +618,7 @@ ALTER TABLE `statistics`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_table_user` (`id`,`account_id`,`deleted`),
   ADD KEY `fk_user_account` (`account_id`),
   ADD KEY `name` (`name`);
 
@@ -448,7 +636,7 @@ ALTER TABLE `whitelist`
 -- AUTO_INCREMENT de la tabla `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2139;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `mail`
@@ -457,10 +645,28 @@ ALTER TABLE `mail`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `mao__claims`
+--
+ALTER TABLE `mao__claims`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mao__deals`
+--
+ALTER TABLE `mao__deals`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mao__offers`
+--
+ALTER TABLE `mao__offers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -501,6 +707,29 @@ ALTER TABLE `inventory_item`
 --
 ALTER TABLE `mail`
   ADD CONSTRAINT `fk_mail` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mao__claims`
+--
+ALTER TABLE `mao__claims`
+  ADD CONSTRAINT `FK_mao__claims_account_id` FOREIGN KEY (`claimer_account_id`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `FK_mao__claims_deal_id` FOREIGN KEY (`deal_id`) REFERENCES `mao__deals` (`id`),
+  ADD CONSTRAINT `FK_mao__claims_user_id` FOREIGN KEY (`claimer_character_id`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `mao__deals`
+--
+ALTER TABLE `mao__deals`
+  ADD CONSTRAINT `FK_account_id` FOREIGN KEY (`publisher_account_id`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `FK_user_id` FOREIGN KEY (`character_id`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `mao__offers`
+--
+ALTER TABLE `mao__offers`
+  ADD CONSTRAINT `FK_mao__offers_account_id` FOREIGN KEY (`offeror_account_id`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `FK_mao__offers_deal_id` FOREIGN KEY (`deal_id`) REFERENCES `mao__deals` (`id`),
+  ADD CONSTRAINT `FK_mao__offers_user_id` FOREIGN KEY (`character_id`) REFERENCES `user` (`id`);
 
 --
 -- Filtros para la tabla `pet`
