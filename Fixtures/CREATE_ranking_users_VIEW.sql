4df09b6e12e8efac3cca775064dd509b8e70ebf1
Recox <lucas.recoaro@gmail.com>
2021-12-03 00:09:05 +1300

    Modifico la view para agregar nuevos parametros al ranking

--- Fixtures/CREATE_ranking_users_VIEW.sql
+++ Fixtures/CREATE_ranking_users_VIEW.sql
@@ -1,1 +1,19 @@
-CREATE VIEW `ranking_users` AS select `u`.`id` AS `id`,`u`.`name` AS `character_name`,`u`.`class_id` AS `class_id`,`u`.`race_id` AS `race_id`,`u`.`genre_id` AS `genre_id`,`u`.`head_id` AS `head_id`,`u`.`level` AS `level`,`u`.`exp` AS `exp`,`u`.`gold` + `u`.`bank_gold` AS `total_gold`,`u`.`criminales_matados` + `u`.`ciudadanos_matados` AS `total_kills`, `u`.`puntos_pesca` AS `puntos_pesca`  from `user` `u` where `u`.`deleted` <> true
+CREATE view `ranking_users`
+AS
+  SELECT `u`.`id`                     AS `id`,
+         `u`.`name`                   AS `character_name`,
+         `u`.`class_id`               AS `class_id`,
+         `u`.`race_id`                AS `race_id`,
+         `u`.`genre_id`               AS `genre_id`,
+         `u`.`head_id`                AS `head_id`,
+         `u`.`level`                  AS `level`,
+         `u`.`exp`                    AS `exp`,
+         `u`.`gold` + `u`.`bank_gold` AS `total_gold`,
+         `u`.`criminales_matados` + `u`.`ciudadanos_matados`   AS `total_kills`,
+         `u`.`criminales_matados`     AS `criminales_matados`,
+         `u`.`ciudadanos_matados`     AS `ciudadanos_matados`,
+         `u`.`puntos_pesca`           AS `puntos_pesca`,
+         `u`.`deaths`                 AS `deaths`,
+         `u`.`killed_npcs`            AS `killed_npcs`
+  FROM   `user` `u`
+  WHERE  `u`.`deleted` <> true  
