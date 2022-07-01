aa8ef42a93fbfaa0a141c5237c5eb36784fda384
MartinTrionfetti <martintrionfetti@gmail.com>
2022-07-01 10:20:00 -0300

    Create 20220628-01-Borro campos facciones inutilizados.sql

---
+++ ScriptsDB/20220628-01-Borro campos facciones inutilizados.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `user` DROP COLUMN pertenece_consejo_real;
+ALTER TABLE `user` DROP COLUMN pertenece_consejo_caos;
+ALTER TABLE `user` DROP COLUMN pertenece_real;
+ALTER TABLE `user` DROP COLUMN pertenece_caos;
