ao-org/argentum-online-server

commit 0aa7b37f6a3979a6bf40e5ed841d4377b8a49451

Feat skins server-side (#995)
* Validate if exists file on txtDimension function to avoid errors.

* Update Server.VBP

Need to migrate all the dependencies to SysWOW64 folder

* Feat skins

* Feat skins

* Update Server.VBP

* Update Declares.bas

* Update Protocol.bas

* Update InvUsuario.bas

* ResetUserSkins

* Fix load char with a skin equipped

* Fix skins equipp during login character

* Update InvUsuario.bas

* Fix skins spells

* Fix desequipar skin hechizos

* Fix skins spells

* Update InvUsuario.bas

* Helmets Skins

* Refactored desequiparSkin

* Fix equipp others skins

* Fix helmets skins

* Feat: Added wings skins

* Feat: Added boats skins

* Fixes on boat skins during loggin

* Fixed helmet skin bug during loggin

* Fixed spells skins desequipando ítem

* Update Modulo_UsUaRiOs.bas

* Update Trabajo.bas

* Create 20250918-01-create table inventory_item_skins

* Fixed .sql file extension of migration file

* Fix: Persistence during adding a new skin

* Fix: MAX_SKINSINVENTORY_SLOTS to 66

* Fix equip ítem after 23 slot

* Removed author comments

* Update CharacterPersistence.bas

* Feat: Delete skin button

* Fix: After deleting a skin, we can add another one. When users relogged's, the skins inventory reorders itself

* Refactor: SkinEquip, DesequiparSkin and CanEquipSkin functions for optimice changeuserChar packets, and fix desequiparSkin bug.

* Refactor: Skins functions for optimizations and bug fix on helmet equip

* Fix: bug on weapons equip

* Fix: boat equipped skin when relogin and doing rare things.

* Fix: an a strange bug of a skin boat

* Update InvUsuario.bas

* Fix loadObjData for skinOrigin

* Update Modulo_UsUaRiOs.bas

* Refactored merge

* Update Modulo_UsUaRiOs.bas

* Update Protocol_Writes.bas

* Update Modulo_UsUaRiOs.bas

* Update modHechizos.bas

* Fix desequip spell skin when inventory is full

* Update modHechizos.bas

* Fixed desequip SkinOrigin

* Update InvUsuario.bas

* Remove comments

* Skins only patreon

Codigo/Declares.bas
@@ -1449,6 +1449,8 @@ Public Const MAX_INVENTORY_OBJS      As Integer = 10000
 ''
 ' Cantidad de "slots" en el inventario con todos los slots desbloqueados
 Public Const MAX_INVENTORY_SLOTS     As Byte = 42
+Public Const MAX_SKINSINVENTORY_SLOTS As Byte = 66
+Public Const MAX_SKINSSPELLS_SLOTS    As Integer = 350
 ' Cantidad de "slots" en el inventario básico
 Public Const MAX_USERINVENTORY_SLOTS As Byte = 24
 ' Cantidad de "slots" en el inventario por fila
@@ -1497,16 +1499,16 @@ Public Enum e_OBJType
     otFullBottle = 34
     otRingAccesory = 35
     otPassageTicket = 36
-    'otLibre = 37
+    otSkinsWings = 37           'Skins de Alas
     otMap = 38
-    'otLibre = 39
-    'otLibre = 40
-    'otLibre = 41
-    'otLibre = 42
-    'otLibre = 43
+    otSkinsArmours = 39         'Skins de Armaduras
+    otSkinsShields = 40         'Skins de Escudos
+    otSkinsHelmets = 41         'Skins de Cascos o Sombreros, o todo lo que vaya en la cabeza
+    otSkinsWeapons = 42         'Skins Armas
+    otSkinsBoats = 43           'Skins Botes, barcas, galeras, galeones,etc
     otSaddles = 44
     otRecallStones = 45
-    'otLibre = 46
+    otSkinsSpells = 46          'Skins de Hechizos
     otMail = 47
     otChest = 48
     otDonator = 50
@@ -1876,6 +1878,13 @@ Public Type t_UserOBJ
     ElementalTags As Long
 End Type
 
+Public Type t_UserSkins
+    Deleted                         As Boolean
+    Equipped                        As Boolean
+    ObjIndex                        As Integer
+    Type                            As Integer
+End Type
+
 Public Type t_Inventario
     Object(1 To MAX_INVENTORY_SLOTS) As t_UserOBJ
     EquippedWeaponObjIndex As Integer
@@ -1901,6 +1910,27 @@ Public Type t_Inventario
     EquippedBackpackObjIndex As Integer
     EquippedBackpackSlot As Byte
     NroItems As Integer
+End Type
+
+Public Type tSkinInventario 'MAX_SKINSINVENTORY_SLOTS
+    'Type debe ir en el Storage Manager pero acá no hace falta, ya está en OBJECT.
+    Object(1 To MAX_SKINSINVENTORY_SLOTS) As t_UserSkins
+    ObjIndexArmourEquipped      As Integer
+    ObjIndexHelmetEquipped      As Integer
+    ObjIndexWeaponEquipped      As Integer
+    ObjIndexShieldEquipped      As Integer
+    ObjIndexWindsEquipped       As Integer
+    ObjIndexBoatEquipped        As Integer
+    ObjIndexBackpackEquipped    As Integer 'Mochila
+    SlotArmourEquipped          As Byte
+    SlotHelmetEquipped          As Byte
+    SlotWeaponEquipped          As Byte
+    SlotShieldEquipped          As Byte
+    SlotWindsEquipped           As Byte
+    SlotBoatEquipped            As Byte
+    SlotBackpackEquipped        As Byte 'Mochila
+    'Spells puede haber varios equipados al mismo tiempo, no tiene sentido.
+    count                       As Byte
 End Type
 
 Public Type t_WorldPos
@@ -2323,6 +2353,7 @@ Public Type t_ObjData
     JineteLevel As Byte
     ElementalTags As Long
     Camouflage As Boolean
+    RequiereObjeto                  As Integer
 End Type
 
 '[Pablo ToxicWaste]
@@ -2432,6 +2463,7 @@ Public Type t_UserStats
     UserAtributos(1 To NUMATRIBUTOS) As Byte
     UserAtributosBackUP(1 To NUMATRIBUTOS) As Byte
     UserHechizos(1 To MAXUSERHECHIZOS) As Integer
+    UserSkinsHechizos(1 To MAX_SKINSSPELLS_SLOTS) As Integer 'No puede ser MAXUSERHECHIZOS porque la cantidad máxima de skins podría escalar en el futuro, debe ser independiente.
     UsuariosMatados As Long
     PuntosPesca As Long
     CriminalesMatados As Long
@@ -2879,6 +2911,7 @@ Public Type t_User
     MENSAJEINFORMACION As String
     invent As t_Inventario
     Invent_bk As t_Inventario
+    Invent_Skins                As tSkinInventario
     pos As t_WorldPos
     ConnectionDetails As t_ConnectionInfo
     CurrentInventorySlots As Byte

Codigo/FileIO.bas
@@ -409,6 +409,7 @@ Public Function TxtDimension(ByVal name As String) As Long
     On Error GoTo TxtDimension_Err
     Dim n As Integer, cad As String, Tam As Long
     n = FreeFile(1)
+    If FileExist(name, vbArchive) Then
     Open name For Input As #n
     Tam = 0
     Do While Not EOF(n)
@@ -416,6 +417,10 @@ Public Function TxtDimension(ByVal name As String) As Long
         Line Input #n, cad
     Loop
     Close n
+    Else
+        Debug.print "No existe el archivo " & name
+    End If
+
     TxtDimension = Tam
     Exit Function
 TxtDimension_Err:
@@ -1036,7 +1041,8 @@ Sub LoadOBJData()
                 Case e_OBJType.otWorkingTools
                     .WeaponAnim = val(Leer.GetValue(ObjKey, "Anim"))
                     .Power = val(Leer.GetValue(ObjKey, "Power"))
-                Case e_OBJType.otArmor
+                Case e_OBJType.otArmor, e_OBJType.otSkinsArmours
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     .Real = val(Leer.GetValue(ObjKey, "Real"))
                     .Caos = val(Leer.GetValue(ObjKey, "Caos"))
                     .LeadersOnly = val(Leer.GetValue(ObjKey, "LeadersOnly")) <> 0
@@ -1048,24 +1054,30 @@ Sub LoadOBJData()
                     Else
                         .velocidad = val(Leer.GetValue(ObjKey, "velocidad"))
                     End If
-                Case e_OBJType.otShield
+                Case e_OBJType.otShield, e_OBJType.otSkinsShields
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     .ShieldAnim = val(Leer.GetValue(ObjKey, "Anim"))
                     .Real = val(Leer.GetValue(ObjKey, "Real"))
                     .Caos = val(Leer.GetValue(ObjKey, "Caos"))
                     .LeadersOnly = val(Leer.GetValue(ObjKey, "LeadersOnly")) <> 0
                     .ResistenciaMagica = val(Leer.GetValue(ObjKey, "ResistenciaMagica"))
                     .Porcentaje = val(Leer.GetValue(ObjKey, "Porcentaje"))
-                Case e_OBJType.otHelmet
+                Case e_OBJType.otHelmet, e_OBJType.otSkinsHelmets
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     .CascoAnim = val(Leer.GetValue(ObjKey, "Anim"))
                     .Real = val(Leer.GetValue(ObjKey, "Real"))
                     .Caos = val(Leer.GetValue(ObjKey, "Caos"))
                     .LeadersOnly = val(Leer.GetValue(ObjKey, "LeadersOnly")) <> 0
                     .ResistenciaMagica = val(Leer.GetValue(ObjKey, "ResistenciaMagica"))
-                Case e_OBJType.otBackpack
+                    
+                Case e_OBJType.otBackpack, e_OBJType.otSkinsWings
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     '.BackpackAnim = val(Leer.GetValue(ObjKey, "Anim"))
+                    
                 Case e_OBJType.otMagicalInstrument
                     .Revive = val(Leer.GetValue(ObjKey, "Revive")) <> 0
-                Case e_OBJType.otWeapon
+                Case e_OBJType.otWeapon, e_OBJType.otSkinsWeapons
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     .WeaponAnim = val(Leer.GetValue(ObjKey, "Anim"))
                     .Apuñala = val(Leer.GetValue(ObjKey, "Apuñala"))
                     .Envenena = val(Leer.GetValue(ObjKey, "Envenena"))
@@ -1125,7 +1137,8 @@ Sub LoadOBJData()
                     .Pimiento = val(Leer.GetValue(ObjKey, "Pimiento"))
                     .SkPociones = val(Leer.GetValue(ObjKey, "SkPociones"))
                     .Porcentaje = val(Leer.GetValue(ObjKey, "Porcentaje"))
-                Case e_OBJType.otShips
+                Case e_OBJType.otShips, e_OBJType.otSkinsBoats
+                    .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
                     .MaxHit = val(Leer.GetValue(ObjKey, "MaxHIT"))
                     .MinHIT = val(Leer.GetValue(ObjKey, "MinHIT"))
                     .velocidad = val(Leer.GetValue(ObjKey, "Velocidad"))
@@ -1227,6 +1240,8 @@ Sub LoadOBJData()
                     .EfectoMagico = val(Leer.GetValue(ObjKey, "efectomagico"))
                 Case e_OBJType.otElementalRune
                     .Hechizo = val(Leer.GetValue(ObjKey, "Hechizo"))
+                Case e_OBJType.otParchment, e_OBJType.otSkinsSpells
+                   .RequiereObjeto = val(Leer.GetValue(ObjKey, "RequiereObjeto"))
             End Select
             .MagicDamageBonus = val(Leer.GetValue(ObjKey, "MagicDamageBonus"))
             .MagicAbsoluteBonus = val(Leer.GetValue(ObjKey, "MagicAbsoluteBonus"))

commit 920df94d6326843ecdd86e511a35dbeed0ca03d8

fix_CasterAndTargetInfoMsgs
Eliminated HechizeroMsg, TargetMsg, and PropioMsg fields from the t_Hechizo type and related file loading logic because they were unused in server.
Restored inside function CastOnUser old message formatting for spell casting feedback (broken in #995 till today)

Codigo/Declares.bas
@@ -1723,9 +1723,6 @@ Public Type t_Hechizo
     nombre As String
     Desc As String
     PalabrasMagicas As String
-    HechizeroMsg As String
-    TargetMsg As String
-    PropioMsg As String
     Tipo As e_TipoHechizo
     SkillType As e_SkillType
     wav As Integer

Codigo/FileIO.bas
@@ -514,9 +514,6 @@ Public Sub CargarHechizos()
         Hechizos(Hechizo).nombre = Leer.GetValue("Hechizo" & Hechizo, "Nombre")
         Hechizos(Hechizo).Desc = Leer.GetValue("Hechizo" & Hechizo, "Desc")
         Hechizos(Hechizo).PalabrasMagicas = Leer.GetValue("Hechizo" & Hechizo, "PalabrasMagicas")
-        Hechizos(Hechizo).HechizeroMsg = Leer.GetValue("Hechizo" & Hechizo, "HechizeroMsg")
-        Hechizos(Hechizo).TargetMsg = Leer.GetValue("Hechizo" & Hechizo, "TargetMsg")
-        Hechizos(Hechizo).PropioMsg = Leer.GetValue("Hechizo" & Hechizo, "PropioMsg")
         Hechizos(Hechizo).NecesitaObj = val(Leer.GetValue("Hechizo" & Hechizo, "NecesitaObj"))
         Hechizos(Hechizo).NecesitaObj2 = val(Leer.GetValue("Hechizo" & Hechizo, "NecesitaObj2"))
         Hechizos(Hechizo).Tipo = val(Leer.GetValue("Hechizo" & Hechizo, "Tipo"))

Codigo/modHechizos.bas
@@ -2501,7 +2501,9 @@ Private Sub InfoHechizo(ByVal UserIndex As Integer)
 
     If IsValidUserRef(UserList(UserIndex).flags.TargetUser) Then
         Call CastOnUser(UserIndex, h, skin, UserList(UserIndex).flags.TargetUser.ArrayIndex)
+        
     ElseIf IsValidNpcRef(UserList(UserIndex).flags.TargetNPC) Then
+    
         Call CastOnNpc(UserIndex, h, skin, UserList(UserIndex).flags.TargetNPC.ArrayIndex)
     Else
         Call CastOnTerrain(UserIndex, h, skin)
@@ -2532,21 +2534,19 @@ End Function
 ' --- Helper 1: Target USER ---
 Private Sub CastOnUser(ByVal UserIndex As Integer, ByVal h As Integer, ByVal skin As Integer, ByVal TargetIndex As Integer)
     On Error GoTo CastOnUser_Err
-
     Dim fxId As Integer
-
     ' FX
     If Hechizos(h).FXgrh > 0 Then
         UserList(TargetIndex).Counters.timeFx = 3
         If Hechizos(h).ParticleViaje > 0 Then
             Call SendData( _
-                SendTarget.ToPCAliveArea, TargetIndex, _
-                PrepareMessageParticleFXWithDestino( _
-                    UserList(UserIndex).Char.charindex, _
-                    UserList(TargetIndex).Char.charindex, _
-                    Hechizos(h).ParticleViaje, Hechizos(h).FXgrh, _
-                    Hechizos(h).TimeParticula, Hechizos(h).wav, 1, _
-                    UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
+               SendTarget.ToPCAliveArea, TargetIndex, _
+               PrepareMessageParticleFXWithDestino( _
+               UserList(UserIndex).Char.charindex, _
+               UserList(TargetIndex).Char.charindex, _
+               Hechizos(h).ParticleViaje, Hechizos(h).FXgrh, _
+               Hechizos(h).TimeParticula, Hechizos(h).wav, 1, _
+               UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
         Else
             If skin > 0 Then
                 fxId = skin
@@ -2554,43 +2554,46 @@ Private Sub CastOnUser(ByVal UserIndex As Integer, ByVal h As Integer, ByVal ski
                 fxId = Hechizos(h).FXgrh
             End If
             Call SendData( _
-                SendTarget.ToPCAliveArea, TargetIndex, _
-                PrepareMessageCreateFX( _
-                    UserList(TargetIndex).Char.charindex, fxId, Hechizos(h).loops, _
-                    UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
+               SendTarget.ToPCAliveArea, TargetIndex, _
+               PrepareMessageCreateFX( _
+               UserList(TargetIndex).Char.charindex, fxId, Hechizos(h).loops, _
+               UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
         End If
     End If
-
     ' Partículas
     If Hechizos(h).Particle > 0 Then
         UserList(TargetIndex).Counters.timeFx = 3
         If Hechizos(h).ParticleViaje > 0 Then
             Call SendData( _
-                SendTarget.ToPCAliveArea, TargetIndex, _
-                PrepareMessageParticleFXWithDestino( _
-                    UserList(TargetIndex).Char.charindex, _
-                    UserList(TargetIndex).Char.charindex, _
-                    Hechizos(h).ParticleViaje, Hechizos(h).Particle, _
-                    Hechizos(h).TimeParticula, Hechizos(h).wav, 0, _
-                    UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
+               SendTarget.ToPCAliveArea, TargetIndex, _
+               PrepareMessageParticleFXWithDestino( _
+               UserList(TargetIndex).Char.charindex, _
+               UserList(TargetIndex).Char.charindex, _
+               Hechizos(h).ParticleViaje, Hechizos(h).Particle, _
+               Hechizos(h).TimeParticula, Hechizos(h).wav, 0, _
+               UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
         Else
             Call SendData( _
-                SendTarget.ToPCAliveArea, TargetIndex, _
-                PrepareMessageParticleFX( _
-                    UserList(TargetIndex).Char.charindex, _
-                    Hechizos(h).Particle, Hechizos(h).TimeParticula, False, , _
-                    UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
+               SendTarget.ToPCAliveArea, TargetIndex, _
+               PrepareMessageParticleFX( _
+               UserList(TargetIndex).Char.charindex, _
+               Hechizos(h).Particle, Hechizos(h).TimeParticula, False, , _
+               UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
         End If
     End If
-
     ' Sonido (sin viaje)
     If Hechizos(h).ParticleViaje = 0 Then
         Call SendData( _
-            SendTarget.ToPCAliveArea, TargetIndex, _
-            PrepareMessagePlayWave(Hechizos(h).wav, _
-                UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
+           SendTarget.ToPCAliveArea, TargetIndex, _
+           PrepareMessagePlayWave(Hechizos(h).wav, _
+           UserList(TargetIndex).pos.x, UserList(TargetIndex).pos.y))
     End If
-
+    If UserIndex = TargetIndex Then
+        Call WriteConsoleMsg(UserIndex, "ProMSG*" & h, e_FontTypeNames.FONTTYPE_FIGHT)
+    Else
+        Call WriteConsoleMsg(UserIndex, "HecMSGU*" & h & "*" & UserList(TargetIndex).name, e_FontTypeNames.FONTTYPE_FIGHT)
+        Call WriteConsoleMsg(TargetIndex, "HecMSGA*" & h & "*" & UserList(UserIndex).name, e_FontTypeNames.FONTTYPE_FIGHT)
+    End If
     ' Efecto de pantalla
     If Hechizos(h).TimeEfect <> 0 Then
         Call WriteFlashScreen(UserIndex, Hechizos(h).ScreenColor, Hechizos(h).TimeEfect)