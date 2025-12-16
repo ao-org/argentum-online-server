ab70ee733753d0b60be5c2a5a017d47cb95a2025
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-12-16 18:25:47 -0300

    Add tables for global quest descriptions and user contributions
    Introduces two new SQL scripts: one to create the 'global_quest_desc' table for storing global quest metadata, and another to create the 'global_quest_user_contribution' table for tracking user contributions to quests. Also adds indexes and foreign key constraints to support efficient queries and data integrity.

---
+++ ScriptsDB/20251216-01-create global quest desc table copy.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "global_quest_desc" (
+	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"  varchar(30) NOT NULL,
+	"obj_id" integer NOT NULL,
+	"counter" integer NOT NULL,
+    "start_date" timestamp NOT NULL DEFAULT current_timestamp,
+    "end_date" timestamp DEFAULT NULL,
+);
+
+
---
+++ ScriptsDB/20251216-02-create global quest user contribution table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "global_quest_user_contribution" (
+	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	"event_id" integer NOT NULL,
+	"user_id"  integer NOT NULL,
+	"timestamp" timestamp NOT NULL DEFAULT current_timestamp,
+	"amount" integer NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (event_id) REFERENCER global_quest_desc(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX idx_user_id ON user(user_id);
+CREATE INDEX idx_event_id ON global_quest_desc(event_id);
