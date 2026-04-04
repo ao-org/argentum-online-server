f2596dd586c217e3a3d281ebd2e1a95e9e941519
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2026-04-04 19:53:37 -0300

    Update 20260402-01-add collectible cards table.sql

--- ScriptsDB/20260402-01-add collectible cards table.sql
+++ ScriptsDB/20260402-01-add collectible cards table.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS "account_collectible_cards" (
     "card_id" integer NOT NULL,
     "rarity" integer NOT NULL,
     "timestamp" timestamp NOT NULL DEFAULT current_timestamp,
+    "quantity" integer NOT NULL DEFAULT 1,
     FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE INDEX idx_account ON account_collectible_cards(account_id);
