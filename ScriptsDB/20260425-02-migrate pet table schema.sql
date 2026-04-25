385fbf968b73acacc2af38f9159fc5d85b10ff7c
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2026-04-25 17:50:40 -0300

    Update 20260425-02-migrate pet table schema.sql

--- ScriptsDB/20260425-02-migrate pet table schema.sql
+++ ScriptsDB/20260425-02-migrate pet table schema.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS "pet" (
-	id SERIAL PRIMARY KEY,
-	user_id	integer NOT NULL,
+	user_id	integer PRIMARY KEY NOT NULL,
 	pet_id1	integer DEFAULT 0,
 	pet_id2	integer DEFAULT 0,
 	pet_id3	integer DEFAULT 0,
