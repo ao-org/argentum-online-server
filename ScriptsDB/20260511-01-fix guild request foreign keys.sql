e624f3844d8fb6fbcad209000ab9b8fb07af9024
Pablo Marquez Tello <gulfas@gmail.com>
2026-05-11 18:16:37 +0100

    Refine guild_request foreign key migration

--- ScriptsDB/20260511-01-fix guild request foreign keys.sql
+++ ScriptsDB/20260511-01-fix guild request foreign keys.sql
@@ -1,13 +1,20 @@
 DROP TABLE IF EXISTS guild_request_new;
 
 CREATE TABLE guild_request_new (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     guild_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     description VARCHAR(512) NOT NULL,
-    CONSTRAINT fk_guild_request_guild_id FOREIGN KEY (guild_id) REFERENCES guilds(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_guild_request_user_id FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (id)
+    CONSTRAINT fk_guild_request_guild_id
+        FOREIGN KEY (guild_id)
+        REFERENCES guilds(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT fk_guild_request_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES "user"(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 INSERT INTO guild_request_new (id, guild_id, user_id, description)
@@ -16,7 +23,7 @@ SELECT gr.id, gr.guild_id, gr.user_id, gr.description
     INNER JOIN "user" u ON u.id = gr.user_id
     INNER JOIN guilds g ON g.id = gr.guild_id;
 
-DROP TABLE guild_request;
+DROP TABLE IF EXISTS guild_request;
 
 ALTER TABLE guild_request_new RENAME TO guild_request;
 
