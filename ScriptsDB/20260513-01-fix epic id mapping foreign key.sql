6f822aa6f01d4fb07ba29480765d1fc90b2130ad
Pablo Marquez Tello <gulfas@gmail.com>
2026-05-13 19:11:57 +0100

    Update 20260513-01-fix epic id mapping foreign key.sql

--- ScriptsDB/20260513-01-fix epic id mapping foreign key.sql
+++ ScriptsDB/20260513-01-fix epic id mapping foreign key.sql
@@ -1,25 +1,9 @@
 DROP TABLE IF EXISTS epic_id_mapping_new;
 
-CREATE TABLE epic_id_mapping_new (
-    epic_id TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    last_login INTEGER DEFAULT NULL,
-    PRIMARY KEY(epic_id),
-    CONSTRAINT fk_epic_id_mapping_user
-        FOREIGN KEY (user_id)
-        REFERENCES "user"(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
+CREATE TABLE epic_id_mapping_new (epic_id varchar(64) NOT NULL, user_id integer NOT NULL, last_login integer NOT NULL, CONSTRAINT fk_epic_id_mapping FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT unique_epic_user_id UNIQUE (epic_id, user_id));
 
-INSERT INTO epic_id_mapping_new (epic_id, user_id, last_login)
-SELECT e.epic_id, e.user_id, e.last_login
-FROM epic_id_mapping e
-INNER JOIN "user" u ON u.id = e.user_id;
+INSERT INTO epic_id_mapping_new (epic_id, user_id, last_login) SELECT epic_id, user_id, last_login FROM epic_id_mapping WHERE user_id IN (SELECT id FROM "user");
 
 DROP TABLE IF EXISTS epic_id_mapping;
 
 ALTER TABLE epic_id_mapping_new RENAME TO epic_id_mapping;
-
-CREATE INDEX IF NOT EXISTS idx_epic_id_mapping_user_id
-ON epic_id_mapping(user_id);
